4de2fb124f84e672d7e824b7224404fd30a3a919
turbou <turbou@i.softbank.jp>
2019-12-19 22:03:38 +0900

    import.

--- sample/dosukoi/admin.py
+++ sample/dosukoi/admin.py
@@ -6,5 +6,15 @@ from .models import Oyoyo
 
 @admin.register(Oyoyo)
 class OyoyoAdmin(admin.ModelAdmin):
-    pass
+    change_form_template = 'admin/my_change_form.html'
+    search_fields = ('name',)
+    list_display = ('name',)
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        self.save_on_top = False
+        self.save_as = False
+        extra_context = extra_context or {}
+        extra_context['can_custom1'] = True
+        extra_context['can_custom2'] = True
+        return super().change_view(request, object_id, form_url, extra_context=extra_context)
 
