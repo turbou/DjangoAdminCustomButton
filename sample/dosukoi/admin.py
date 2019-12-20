cec95b6b7680789103faa83dfd75ef41c4cc8ec0
turbou <turbou@i.softbank.jp>
2019-12-21 02:29:32 +0900

    update.

--- sample/dosukoi/admin.py
+++ sample/dosukoi/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django import forms
 from django.utils.safestring import mark_safe
+from django.urls import reverse
 
 from .models import Oyoyo
 
@@ -8,15 +9,25 @@ from .models import Oyoyo
 class OyoyoAdmin(admin.ModelAdmin):
     change_form_template = 'admin/my_change_form.html'
     search_fields = ('name',)
-    list_display = ('operation', 'name',)
+    list_display = ('operation', 'name', 'app_status')
+    list_filter = ('app_status',)
+    fieldsets = [
+        (None, {'fields': [
+            ('name', 'app_status'),
+        ]}),
+    ]
 
     def operation(self, obj):
+        url = reverse('admin:%s_%s_change' % (obj._meta.app_label, obj._meta.model_name), args=[obj.id])
         str_buffer = []
-        str_buffer.append('<button type="button" class="btn btn-sm btn-outline-secondary" title="修正" name="edit" value="0">修正</button>')
-        str_buffer.append('<button type="button" class="btn btn-sm btn-outline-secondary" title="更新承認" name="edit" value="0">更新承認</button>')
-        str_buffer.append('<button type="button" class="btn btn-sm btn-outline-secondary" title="営業承認" name="edit" value="0">営業承認</button>')
+        str_buffer.append('<button type="button" class="btn btn-sm btn-outline-secondary" title="修正" name="edit" onclick="location.href=\'%s?mode=update\'">修正</button>' % url)
+        str_buffer.append('<button type="button" class="btn btn-sm btn-outline-secondary" title="更新承認" name="edit" onclick="location.href=\'%s?mode=upauth\'">更新承認</button>' % url)
+        str_buffer.append('<button type="button" class="btn btn-sm btn-outline-secondary" title="営業承認" name="edit" onclick="location.href=\'%s?mode=bzauth\'">営業承認</button>' % url)
         return mark_safe('<div class="bootstrap">%s</div>' % ('&nbsp;&nbsp;'.join(str_buffer)))
     operation.short_description = '操作'
+
+    def get_list_display_links(self, request, list_display):
+        return None
 
     def change_view(self, request, object_id, form_url='', extra_context=None):
         self.save_on_top = False
--- sample/dosukoi/models.py
+++ sample/dosukoi/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
+APP_STATUS_CHOICES = (
+    ('W', '承認待ち'),
+    ('U', '更新承認済み'),
+    ('B', '営業承認済み'),
+)
+
 class Oyoyo(models.Model):
     name = models.CharField('名前', max_length=30)
+    app_status = models.CharField('承認ステータス', choices=APP_STATUS_CHOICES, max_length=1, default='W')
 
     def __str__(self):
         return '%s' % (self.name)
