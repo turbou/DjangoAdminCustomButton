81346b24effb5ebc408eb637447162a9923e5706
turbou <turbou@i.softbank.jp>
2019-12-23 01:53:51 +0900

    update.

--- sample/static/admin/js/confirm.js
+++ sample/static/admin/js/confirm.js
@@ -1,4 +1,7 @@
 $(document).on('click', '[id="update"]', function(){
+  var btn = document.getElementById('update');
+  $('#exampleModal #exampleModalLabel').text(btn.value);
+  $('#exampleModal #exampleModalBody').text(btn.value + 'します。よろしいですか？');
   $('#exampleModal').modal('show')
 }); 
 $(document).on('click', '[id="submit"]', function(){
